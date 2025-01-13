2ed7cf5f160c615eb35b00a89c79c17f4bc678e8
皮皮强 <ycqycqycqycq@126.com>
2025-01-13 17:25:18 +0800

    Update app.go

--- app/utils/app/app.go
+++ app/utils/app/app.go
@@ -3,19 +3,19 @@ package app
 import (
 	"sync"
 
-	"github.com/pocketbase/pocketbase"
+	// "github.com/pocketbase/pocketbase"
 )
 
 var instance *pocketbase.PocketBase
 
-var intanceOnce sync.Once
+// var intanceOnce sync.Once
 
 func GetApp() *pocketbase.PocketBase {
-	intanceOnce.Do(func() {
-		instance = pocketbase.NewWithConfig(pocketbase.Config{
-			HideStartBanner: true,
-		})
-	})
+	// intanceOnce.Do(func() {
+	// 	instance = pocketbase.NewWithConfig(pocketbase.Config{
+	// 		HideStartBanner: true,
+	// 	})
+	// })
 
 	return instance
 }
